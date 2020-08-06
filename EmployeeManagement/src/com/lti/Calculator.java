ca32e24af8e6e2ed089203c37d16d7bf76e269ae
Aditya Srivastava <aadisrivastava.ams@gmail.com>
2020-08-06 09:52:37 +0530

    Added diff function

--- EmployeeManagement/src/com/lti/Calculator.java
+++ EmployeeManagement/src/com/lti/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 	public void add(int a, int b) {
 		System.out.println("Sum="+(a+b));
 	}
+	
+	public void diff(int a, int b) {
+		System.out.println("Diff="+(a-b));
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
