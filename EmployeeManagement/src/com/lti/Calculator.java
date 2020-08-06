489ea29d856a23bc871e0d598b4feb0c7d6acfc3
Aditya Srivastava <aadisrivastava.ams@gmail.com>
2020-08-06 10:20:59 +0530

    product function added by me

--- EmployeeManagement/src/com/lti/Calculator.java
+++ EmployeeManagement/src/com/lti/Calculator.java
@@ -9,6 +9,9 @@ public class Calculator {
 	public void diff(int a, int b) {
 		System.out.println("Diff="+(a-b));
 	}
+	public void product(int a, int b) {
+		System.out.println("Product="+(a*b));
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
